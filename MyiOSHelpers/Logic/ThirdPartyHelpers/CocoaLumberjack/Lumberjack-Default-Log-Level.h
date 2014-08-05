9eac1b025f1ad28909dedd406abc176d4a415957
Vincil Bishop <vincil.bishop@vbishop.com>
2014-08-05 17:15:28 -0500

    Updated log levels

--- MyiOSHelpers/Logic/ThirdPartyHelpers/CocoaLumberjack/Lumberjack-Default-Log-Level.h
+++ MyiOSHelpers/Logic/ThirdPartyHelpers/CocoaLumberjack/Lumberjack-Default-Log-Level.h
@@ -6,6 +6,14 @@
 
 #if defined(DEBUG) || defined(LUMBERJACK_VERBOSE)
 static int ddLogLevel = LOG_LEVEL_VERBOSE;
+#elif defined(LUMBERJACK_DEBUG)
+static int ddLogLevel = LOG_LEVEL_DEBUG;
+#elif defined(LUMBERJACK_INFO)
+static int ddLogLevel = LOG_LEVEL_INFO;
+#elif defined(LUMBERJACK_WARN)
+static int ddLogLevel = LOG_LEVEL_WARN;
+#elif defined(LUMBERJACK_ERROR)
+static int ddLogLevel = LOG_LEVEL_ERROR;
 #else
 static int ddLogLevel = LOG_LEVEL_OFF;
 #endif
